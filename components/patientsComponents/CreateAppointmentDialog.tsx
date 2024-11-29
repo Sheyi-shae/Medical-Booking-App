31710b3c4424525e444937f859ae1cbfd41e4a09
Seyi Adeniyi <adeniyiemmanuel042@gmail.com>
2024-11-29 12:16:02 +0100

    jrnfur

--- components/patientsComponents/CreateAppointmentDialog.tsx
+++ components/patientsComponents/CreateAppointmentDialog.tsx
@@ -28,7 +28,7 @@ export function CreateAppointDialog({doctors=[]}:DoctorsFilter) {
          text-sm px-5 py-2.5 text-center me-2 mb-2">Book Appointment</Button>
      
           
-          <Modal show={openModal} size="lg" onClose={onCloseModal} popup>
+          {/* <Modal show={openModal} size="lg" onClose={onCloseModal} popup>
         <Modal.Header />
         
         <div className="text-blue-500 h-auto">Create an Appointment with a Specialist</div>
@@ -38,7 +38,29 @@ export function CreateAppointDialog({doctors=[]}:DoctorsFilter) {
             
             
         </Modal.Body>
-      </Modal>
+      </Modal> */}
+      <Modal show={openModal} onClose={() => setOpenModal(false)}>
+      <Modal.Header>Terms of Service</Modal.Header>
+      <Modal.Body>
+        <div className="space-y-6">
+          <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+            With less than a month to go before the European Union enacts new consumer privacy laws for its citizens,
+            companies around the world are updating their terms of service agreements to comply.
+          </p>
+          <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+            The European Union’s General Data Protection Regulation (G.D.P.R.) goes into effect on May 25 and is meant
+            to ensure a common set of data rights in the European Union. It requires organizations to notify users as
+            soon as possible of high-risk data breaches that could personally affect them.
+          </p>
+        </div>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button onClick={() => setOpenModal(false)}>I accept</Button>
+        <Button color="gray" onClick={() => setOpenModal(false)}>
+          Decline
+        </Button>
+      </Modal.Footer>
+    </Modal>
     </>
   )
 }
